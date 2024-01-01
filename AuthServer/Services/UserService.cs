0ba7b7a21d4754f8f0cf56884f63c145973f08e6
shyam.rahangdale <shyam.rahangdale@csm.tech>
2024-01-01 19:13:00 +0530

    ok

--- AuthServer/Services/UserService.cs
+++ AuthServer/Services/UserService.cs
@@ -225,18 +225,18 @@ namespace AuthServer.Services
 			}).ToList();
 		}
 
-		public DataSet ConvertDataReaderToDataSet(IDataReader data)
-		{
-			DataSet ds = new DataSet();
-			int i = 0;
-			while (!data.IsClosed)
-			{
-				ds.Tables.Add("Table" + (i + 1));
-				ds.EnforceConstraints = false;
-				ds.Tables[i].Load(data);
-				i++;
-			}
-			return ds;
-		}
+		//public DataSet ConvertDataReaderToDataSet(IDataReader data)
+		//{
+		//	DataSet ds = new DataSet();
+		//	int i = 0;
+		//	while (!data.IsClosed)
+		//	{
+		//		ds.Tables.Add("Table" + (i + 1));
+		//		ds.EnforceConstraints = false;
+		//		ds.Tables[i].Load(data);
+		//		i++;
+		//	}
+		//	return ds;
+		//}
 	}
 }
