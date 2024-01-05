3b4cd71c253b65b7154c8ba5cfd96d4f103c4302
shyam.rahangdale <shyam.rahangdale@csm.tech>
2024-01-05 17:00:14 +0530

    ocelot created

--- AdminPanelAPIGateway/Program.cs
+++ AdminPanelAPIGateway/Program.cs
@@ -1,7 +1,57 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
 
+var builder = WebApplication.CreateBuilder(args);
+//add cross
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+
+	options.AddPolicy(MyAllowSpecificOrigins,
+	builder =>
+	{
+		builder.AllowAnyOrigin()
+   .AllowAnyMethod()
+		.AllowAnyHeader();
+	});
+});
+
+//add auth
+var audienceConfig = builder.Configuration.GetSection("Audience");
+var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(audienceConfig["Secret"]));
+
+builder.Services.AddAuthentication(options =>
+{
+	//options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+			   .AddJwtBearer(jwtBearerOptions =>
+			   {
+				   jwtBearerOptions.RequireHttpsMetadata = true;
+				   jwtBearerOptions.SaveToken = true;
+				   jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+				   {
+					   ValidateIssuerSigningKey = true,
+					   IssuerSigningKey = signingKey,
+					   ValidateIssuer = false,
+					   ValidateAudience = false,
+					   ClockSkew = TimeSpan.Zero
+				   };
+			   });
+
+//from 5.0 auth app
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+//ocelot
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -14,8 +64,16 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//from 5.0 auth app
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseRouting();
+app.UseAuthentication();
+//from 5.0 auth app
+
+
 app.UseAuthorization();
 
-app.MapRazorPages();
 
+await app.UseOcelot();
 app.Run();
