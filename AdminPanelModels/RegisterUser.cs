4ad931551c965c48587f94121858c6be22a9cde5
shyam.rahangdale <shyam.rahangdale@csm.tech>
2024-02-04 02:31:35 +0530

    Registration in progress

--- AdminPanelModels/RegisterUser.cs
+++ AdminPanelModels/RegisterUser.cs
@@ -13,54 +13,91 @@ namespace AdminPanelModels
 		
 		[EmailAddress(ErrorMessage = "Invalid Email Address !")]
 		[Display(Name = "Email Id")]
+        [UniqueEmail(ErrorMessage = "Email already exists")]
         public string EmailId { get; set; }
 
         [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number !")]
-		public string Mobile_No { get; set; }
-		[Required]
+		public string? Mobile_No { get; set; }
+		
+        //[Required]
 		[StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-		//[Remote("CheckUserExist", "AddUser", ErrorMessage = "Username already exist try another name !")]
-		[RegularExpression("^[a-zA-Z]\\w+|[0-9][0-9_]*[a-zA-Z]+\\w*$", ErrorMessage = "Please follow username policy.")]
+       
+        //[Remote("CheckUserExist", "AddUser", ErrorMessage = "Username already exist try another name !")]
+        [RegularExpression("^[a-zA-Z]\\w+|[0-9][0-9_]*[a-zA-Z]+\\w*$", ErrorMessage = "Please follow username policy.")]
 		[Display(Name = "User Name")]
-		public string username { get; set; }
+		public string? username { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
-
         [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,15}$", ErrorMessage = "Please follow password policy.minimum of 1 lower case letter,a minimum of 1 upper case letter, a minimum of 1 numeric character,a minimum of 1 special character")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Required]
-		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
-
-		[RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,15}$", ErrorMessage = "Please follow password policy.minimum of 1 lower case letter,a minimum of 1 upper case letter, a minimum of 1 numeric character,a minimum of 1 special character")]
 		[DataType(DataType.Password)]
 		[Display(Name = "Confirm Password")]
-		public string PD_Reenterpwd { get; set; }
+        [Compare("Password",ErrorMessage = "Confirm Password not match")]
+        public string PD_Reenterpwd { get; set; }
 
 
 
-        [Required]
+        //[Required]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        //[Remote("CheckUserExist", "AddUser", ErrorMessage = "Username already exist try another name !")]
         [RegularExpression("^[a-zA-Z]\\w+|[0-9][0-9_]*[a-zA-Z]+\\w*$", ErrorMessage = "Please follow username policy.")]
         [Display(Name = "Organization Name")]
-        public string OrganizationName { get; set; }
+        public string? OrganizationName { get; set; }
 
-        [Required]
-        [RegularExpression(@"^([0-9])$", ErrorMessage = "Invalid Application !")]
-        public string Iid { get; set; }
+        //[Required]
+        //[RegularExpression(@"^([0-9])$", ErrorMessage = "Invalid Application !")]
+        public string? Iid { get; set; }
 
-        public IBrowserFile userphoto { get; set; }
+        public IBrowserFile? userphoto { get; set; }
 
         [Required]
 		[RegularExpression(@"^([0-9]{6})$", ErrorMessage = "Invalid OTP !")]
-		public string otp { get; set; }
+        [VerifyOTP(ErrorMessage = "Input Correct OTP")] 
+        public string otp { get; set; }
 
-       
+    }
+    public class UniqueEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+          
+            var email = (string)value;
 
+            string userid = null;
+            // string userid = "1"; then  message will display
 
+            var existingUser = userid;
+
+            if (existingUser != null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+    public class VerifyOTPAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            var email = (string)value;
+
+            string otp = null;
+            // string otp = "1"; then  message will display
+
+            var verifyotp = otp;
+
+            if (verifyotp != null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
