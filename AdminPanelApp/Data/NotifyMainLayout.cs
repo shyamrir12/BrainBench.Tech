65c0ad821de7df01bff4df47b516414f9a450450
shyam.rahangdale <shyam.rahangdale@csm.tech>
2024-02-05 01:13:42 +0530

    Log in progress

---
+++ AdminPanelApp/Data/NotifyMainLayout.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminPanelApp.Data
+{
+    public class NotifyMainLayout
+    {
+        public bool SpinnerVisible { get; set; } = false;
+        public bool DroverTopBarVisible { get; set; } = true;
+    }
+}
--- AdminPanelModels/RecoverPassword.cs
+++ AdminPanelModels/RecoverPassword.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,14 @@ using System.Threading.Tasks;
 namespace AdminPanelModels
 {
 	public class RecoverPassword
-	{
-	}
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address !")]
+        [Display(Name = "Email Id")]
+        public string EmailId { get; set; }
+
+        [Required]
+        [Display(Name = "Captcha")]
+        public string captcha { get; set; }
+    }
 }
