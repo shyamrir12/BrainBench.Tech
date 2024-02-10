e63069a2eb77066a4721650b2c26b7d0924e56fe
shyam.rahangdale <shyam.rahangdale@csm.tech>
2024-02-11 01:48:43 +0530

    design in progress

--- AdminPanelModels/UserMangment/AddUserModel.cs
+++ AdminPanelModels/UserMangment/AddUserModel.cs
@@ -7,30 +7,30 @@ using System.Threading.Tasks;
 
 namespace AdminPanelModels.UserMangment
 {
-    public class AddUser
+    public class AddUserModel
     {
 
         public string roledesc { get; set; }
-        [Required]
+       // [Required]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string name { get; set; }
-        [Required]
+       // [Required]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         //[Remote("CheckUserExist", "AddUser", ErrorMessage = "Username already exist try another name !")]
         [RegularExpression("^[a-zA-Z]\\w+|[0-9][0-9_]*[a-zA-Z]+\\w*$", ErrorMessage = "Please follow username policy.")]
         [Display(Name = "User Name")]
         public string username { get; set; }
 
-        [Required]
+       // [Required]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string designation { get; set; }
-        [Required]
+     //   [Required]
         [EmailAddress(ErrorMessage = "Invalid Email Address !")]
         [Display(Name = "Email Id")]
         public string email { get; set; }
         public int userid { get; set; }
 
-        [Required]
+      //  [Required]
         [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number !")]
         [Display(Name = "Mobile No")]
         public string mobile_no { get; set; }
@@ -49,7 +49,7 @@ namespace AdminPanelModels.UserMangment
 
 
 
-        [Required]
+     //   [Required]
         [Display(Name = "Captcha")]
         public string captcha { get; set; }
     }
